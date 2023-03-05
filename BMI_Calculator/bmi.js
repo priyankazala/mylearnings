1dea3a88301321cce5765d496a61dd66624c3cf1
zpriyanka <115831835+priyankazala@users.noreply.github.com>
2023-03-05 14:17:49 -0500

    Initial commit

---
+++ BMI_Calculator/bmi.js
@@ -0,0 +1,43 @@
+function BMICalculator(height,weight)
+{
+    let h= height/100;
+    let sqHeight = h*h;
+
+    let bmiValue = weight/sqHeight;
+    let bmiValueRounded = bmiValue.toFixed(1);
+    
+    return bmiValueRounded
+
+
+};
+
+function calc(){
+let h = document.getElementById("height").value
+let w = document.getElementById("weight").value
+//document.getElementById("height").innerHTML =h;
+//document.getElementById("weight").innerHTML = w;
+//console.log(h);
+//console.log(w);
+if(h!= "" && w!= ""){
+let result=BMICalculator(h,w);
+
+if(result<=18.5){
+    document.getElementById("result").innerHTML ="you are underweight";
+    
+    console.log("you are underweight");
+}
+else if(18.5<result && result<=25){
+    console.log("you are healthy. Congratulations!!");
+    document.getElementById("result").innerHTML ="you are healthy. Congratulations!!";
+
+}
+else if(25<result && result<=30){
+    console.log("you are overweight.");
+    document.getElementById("result").innerHTML ="you are overweight.";
+}
+else{
+    console.log("you are obese.")
+    document.getElementById("result").innerHTML ="you are obese.";
+}
+}
+}
